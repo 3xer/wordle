2143f6b805f95634b755c5daa90dc08929c544eb
3xer <pontuslundholm1@gmail.com>
2023-03-21 11:24:39 +0100

    full algorithm W tests

--- index.js
+++ index.js
@@ -1,53 +1,46 @@
-
-
-
-export default function wordle(input, answer) {
-  const outArray = [];
-
-  if (input.length == 5) {
-
-    // loop to find if any letters match
-    for (let i = 0; i < 5; i++) {
-
-      //if answer includes a letter from the input
-      if (answer.includes(input.charAt(i))) {
-
-        if (answer.charAt(i) == input.charAt(i)) {
-          console.log('Letter ' + answer.charAt(i) +' is correct at ' + i)
-          const match = {
-            letter: input.charAt(i),
-            result: "correct",
-          };
-
-          if(outArray.includes(match.letter)){
-            console.log('think dubble letter')
-          }
-          outArray.push(match);
-        } 
-        
-        else {
-          console.log('bokstav:' + input.charAt(i) + ' finns ' + i)
-          const match = {
-            letter: input.charAt(i),
-            result: "misplaced",
-          };
-          if(outArray.includes(match.letter)){
-            console.log('think dubble letter')
-          }
-          outArray.push(match);
-          
-        }
-        
+export default function wordle(guess, rightAnswer) {
+  const input = guess.toUpperCase().split("");
+  let answer = rightAnswer.toUpperCase().split("");
+  let outArray = guess.toUpperCase().split("");
+  let correctLetters = [];
+  for (let i = 0; i < input.length; i++) {
+    const inCorrect = {
+      letter: input[i],
+      result: "incorrect",
+    };
+    const correct = {
+      letter: input[i],
+      result: "correct",
+    };
+    if (answer.includes(input[i])) {
+      if (outArray[i] == answer[i]) {
+        outArray.splice(i, 1, correct);
+        correctLetters.push(input[i]);
       }
+    } else {
+      outArray.splice(i, 1, inCorrect);
     }
   }
   for (let i = 0; i < outArray.length; i++) {
-
-
-    if(outArray[i].letter == outArray[i]){
-        console.log(outArray)
+    const inCorrect = {
+      letter: input[i],
+      result: "incorrect",
+    };
+    if (correctLetters.includes(outArray[i])) {
+      outArray.splice(i, 1, inCorrect);
     }
   }
-    console.log(outArray)
-    return outArray;
+  for (let i = 0; i < outArray.length; i++) {
+    const missplaced = {
+      letter: input[i],
+      result: "missplaced",
+    };
+    if (answer.includes(outArray[i])) {
+      outArray.splice(i, 1, missplaced);
+    }
+  }
+  console.log(outArray);
+  return outArray;
 }
+wordle("hallå", "cykla");
+
--- index.spec.js
+++ index.spec.js
@@ -5,9 +5,58 @@ import {
   } from "@jest/globals";
   import wordle from "./index.js"
 
-describe('describe', () => {
+describe('Base tests to make sure the algorithm works as intended', () => {
 
-    test('is cykla == cykla', () => {
-        const res = wordle("cykla", "hallå")
+    test('Are output in the initial order and capital letters ', () => {
+        const res = wordle("abcde", "abcde")
+        expect(res[0].letter).toBe('A')
+        expect(res[1].letter).toBe('B')
+        expect(res[2].letter).toBe('C')
+        expect(res[3].letter).toBe('D')
+        expect(res[4].letter).toBe('E')
+        
+    })
+    test('hallå != cykla', () => {
+        const res = wordle("hallå", "cykla")
+        expect(res[0].result).toBe('incorrect')
+        expect(res[1].result).toBe('missplaced')
+        expect(res[2].result).toBe('incorrect')
+        expect(res[3].result).toBe('correct')
+        expect(res[4].result).toBe('incorrect')
+
+        expect(res[0].letter).toBe('H')
+        expect(res[1].letter).toBe('A')
+        expect(res[2].letter).toBe('L')
+        expect(res[3].letter).toBe('L')
+        expect(res[4].letter).toBe('Å')
+    })
+    test('result of 5 L on cykla should show four incorrect and one correct at index 3', () => {
+        const res = wordle("lllll", "cykla")
+        expect(res[0].result).toBe('incorrect')
+        expect(res[1].result).toBe('incorrect')
+        expect(res[2].result).toBe('incorrect')
+        expect(res[3].result).toBe('correct')
+        expect(res[4].result).toBe('incorrect')
+
+        expect(res[0].letter).toBe('L')
+        expect(res[1].letter).toBe('L')
+        expect(res[2].letter).toBe('L')
+        expect(res[3].letter).toBe('L')
+        expect(res[4].letter).toBe('L')
+    })
+    test('input and aswer is the same result in shoud all be correct and letters inte the same order', () => {
+        const res = wordle("cykla", "cykla")
+        expect(res[0].result).toBe('correct')
+        expect(res[1].result).toBe('correct')
+        expect(res[2].result).toBe('correct')
+        expect(res[3].result).toBe('correct')
+        expect(res[4].result).toBe('correct')
+        
+        expect(res[0].letter).toBe('C')
+        expect(res[1].letter).toBe('Y')
+        expect(res[2].letter).toBe('K')
+        expect(res[3].letter).toBe('L')
+        expect(res[4].letter).toBe('A')
+
     })
 })
